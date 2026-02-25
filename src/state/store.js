c61be309ce64669da9e5cd7358ea3e600cf581aa
Kratikap23 <kratikap665@gmail.com>
2026-02-25 21:06:16 +0530

    Correction

--- src/state/store.js
+++ src/state/store.js
@@ -1,7 +1,6 @@
 import { applyMiddleware, createStore } from "redux";
 import reducers from "./reducers";
-import { thunk } from "redux-thunk";   // ✅ named import
-
+import { thunk } from "redux-thunk";  
 export const store = createStore(
   reducers,
   {},
